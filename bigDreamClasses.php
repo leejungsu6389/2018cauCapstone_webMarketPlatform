a3ceb87a80347f22540c1aba9bbbb4ec8a243203
leejungsu <33692466+leejungsu6389@users.noreply.github.com>
2018-04-16 18:09:46 +0900

    Add files via upload

---
+++ bigDreamClasses.php
@@ -0,0 +1,342 @@
+﻿<?php
+
+	/* ============================================ */
+	/* User Class */
+	class User {
+
+		private $id;
+		private $pswd;
+		private $date;
+		private $name;
+		private $zipCode;
+		private $address;
+		private $email;
+		private $telNum;
+		private $point;
+
+
+		/* constructor */
+		function __constructor(){
+						
+		}
+    
+  
+		/* setter */
+		public function setID($input){
+			$this->id = $input;
+		}
+    
+		public function setPswd($input){
+			$this->pswd = $input;
+		}
+    
+		public function setDate($input){
+			$this->date = $input;
+		}
+    
+		public function setName($input){
+			$this->name = $input;
+		}
+    
+		public function setZipCode($input){
+			$this->zipCode = $input;
+		}
+    
+		public function setAddress($input){
+			$this->address = $input;
+		}
+    
+		public function setEmail($input){
+			$this->email = $input;
+		}
+    
+		public function setTelNum($input){
+			$this->telNum = $input;
+		}
+    
+  		public function setPoint($input){
+			$this->point = $input;
+		}
+    
+    
+		/* getter */
+		public function getID(){
+			return $this->id;
+		}
+
+		public function getPswd(){
+			return $this->pswd;
+		}
+		
+		public function getDate(){
+			return $this->date;
+		}
+		
+		public function getName(){
+			return $this->name;
+		}
+		
+		public function getZipCode(){
+			return $this->zipCode;
+		}
+		
+		public function getAddress(){
+			return $this->address;
+		}
+		
+		public function getEmail(){
+			return $this->email;
+		}
+		
+		public function getTelNum(){
+			return $this->telNum;
+		}
+		
+		public function getPoint(){
+			return $this->point;
+		}
+	
+	}
+
+
+
+
+
+	/* ============================================ */
+	/* admin Class */
+
+	class Admin {
+
+		private $id;
+		private $pswd;
+		private $authority;
+		
+    
+    function __constructor(){
+						
+		}
+    
+ 
+		public function setID($input){
+			$this->id = $input;
+		}
+    
+		public function setPswd($input){
+			$this->pswd = $input;
+		}
+    
+		public function setAuthority($input){
+			$this->authority = $input;
+		}
+    
+		
+		public function getId(){
+			return $this->id;
+		}
+
+		public function getPswd(){
+			return $this->pswd;
+		}
+		
+		public function getAuthority(){
+			return $this->authority;
+		}	
+	}
+
+
+
+	/* ============================================ */
+	/* Item Class */
+
+	class Item {
+
+		private $itemID;
+		private $itemName;
+		private $price;
+		private $category;
+		private $company;
+		private $description;
+	
+
+    function __constructor(){
+						
+		}
+    
+    
+    public function setItemID($input){
+			$this->itemID = $input;
+		}
+    
+		public function setItemName($input){
+			$this->itemName = $input;
+		}
+    
+		public function setPrice($input){
+			$this->price = $input;
+		}
+    
+    public function setCategory($input){
+			$this->category = $input;
+		}
+    
+		public function setCompany($input){
+			$this->company = $input;
+		}
+    
+		public function setDescription($input){
+			$this->description = $input;
+		}
+		
+    
+    public function getItemID(){
+			return $this->itemID;
+		}
+    
+		public function getItemName(){
+			return $this->itemName;
+		}
+
+		public function getPrice(){
+			return $this->price;
+		}
+
+		public function getCategory(){
+			return $this->category;
+		}
+		public function getCompany(){
+			return $this->company;
+		}
+		public function getDescription(){
+			return $this->description;
+		}
+	}
+
+
+
+	/* ============================================ */
+	/* cart Class */
+
+	class Cart {
+
+		private $cost;
+		private $itemCode;
+		private $quantity;
+	
+
+    function __constructor(){
+						
+		}
+		
+    
+		public function setCost($input){
+			$this->cost = $input;
+		}
+    
+		public function setItemCode($input){
+			$this->ItemCode = $input;
+		}
+    
+		public function setQuantity($input){
+			$this->quantity = $input;
+		}
+    		
+		
+		public function getCost(){
+			return $this->cost;
+		}
+
+		public function getItemCode(){
+			return $this->itemCode;
+		}
+		
+		public function getQuantity(){
+			return $this->quantity;
+		}
+	}
+
+
+
+	/* ============================================ */
+	/* order Class */
+
+	class Order {
+
+		private $orderID;
+		private $itemList;
+		private $receiverName;
+		private $receiverTelNum;
+		private $receiverAddr;
+		private $receiverAddr2;
+		private $deliveryMessage;
+		private $paymentType;
+
+
+    function __constructor(){
+						
+		}		
+
+    
+		public function setOrderID($input){
+			$this->orderID = $input;
+		}
+    
+		public function setItemList($input){
+			$this->itemList = $input;
+		}
+    
+		public function setReceiverName($input){
+			$this->receiverName = $input;
+		}
+    		public function setReceiverTelNum($input){
+			$this->receiverTelNum = $input;
+		}
+    
+		public function setReceiverAddr($input){
+			$this->receiverAddr = $input;
+		}
+    
+		public function setReceiverAddr2($input){
+			$this->receiverAddr2 = $input;
+		}
+    		public function setDeliveryMessage($input){
+			$this->deliveryMessage = $input;
+		}
+    
+		public function setPaymentType($input){
+			$this->paymentType = $input;
+		}
+    		
+
+		public function getOrderID(){
+			return $this->orderID;
+		}
+
+		public function getItemList(){
+			return $this->itemList;
+		}
+		
+		public function getReceiverName(){
+			return $this->receiverName;
+		}
+		public function getReceiverTelNum(){
+			return $this->receiverTelNum;
+		}
+
+		public function getReceiverAddr(){
+			return $this->receiverAddr;
+		}
+		
+		public function getReceiverAddr2(){
+			return $this->receiverAddr2;
+		}
+		public function getDeliveryMessage(){
+			return $this->deliveryMessage;
+		}
+
+		public function getPaymentType(){
+			return $this->paymentType;
+		}
+	}
+
+
+?>
---
+++ server_test.php
@@ -0,0 +1,176 @@
+﻿<?php
+
+  /* import classes */
+  require_once("./bigDreamClasses.php");
+
+  /* server decides the action with this variable */
+  /* server acts with many if-else statements */
+  $_WHAT_TO_DO = $_POST["_WHAT_TO_DO"];
+
+
+
+  /* ============================================ */
+  /* item management */
+  
+  /* add item */
+  if($_WHAT_TO_DO == 'ADD_ITEM'){
+  
+    /* create new object */
+    $itemO = new Item();
+    
+    /* set object member variable */
+    $itemO->setItemID($_POST["_itemID"]);
+    $itemO->setItemName($_POST["_itemName"]);
+    $itemO->setPrice($_POST["_price"]);
+    $itemO->setCategory($_POST["_categoryID"]);
+    $itemO->setCompany($_POST["_description"]);
+    $itemO->setDescription($_POST["_company"]);
+  
+  
+    echo "<br/>----------Unit Test----------";
+	  echo "<br/>"."What To Do: ".$_WHAT_TO_DO;
+	  echo "<br/>".$itemO->getItemID();
+	  echo "<br/>".$itemO->getItemName();
+	  echo "<br/>".$itemO->getPrice();
+	  echo "<br/>".$itemO->getCategory();
+	  echo "<br/>".$itemO->getCompany();
+	  echo "<br/>".$itemO->getDescription();
+  
+  }
+
+
+  /* Edit item */
+  else if($_WHAT_TO_DO == 'EDIT_ITEM'){
+  
+    /* create new object */
+    $itemO = new Item();
+    
+    /* set object member variable */
+    $itemO->setItemID($_POST["_itemID"]);
+    $itemO->setItemName($_POST["_itemName"]);
+    $itemO->setPrice($_POST["_price"]);
+    $itemO->setCategory($_POST["_categoryID"]);
+    $itemO->setCompany($_POST["_description"]);
+    $itemO->setDescription($_POST["_company"]);
+  
+  
+    echo "<br/>----------Unit Test----------";
+	  echo "<br/>"."What To Do: ".$_WHAT_TO_DO;
+	  echo "<br/>".$itemO->getItemID();
+	  echo "<br/>".$itemO->getItemName();
+	  echo "<br/>".$itemO->getPrice();
+	  echo "<br/>".$itemO->getCategory();
+	  echo "<br/>".$itemO->getCompany();
+	  echo "<br/>".$itemO->getDescription();
+  
+  }
+
+
+  /* Del item */
+  else if($_WHAT_TO_DO == 'DEL_ITEM'){
+  
+    /* create new object */
+    $itemO = new Item();
+    
+    /* set object member variable */
+    $itemO->setItemID($_POST["_itemID"]);
+  
+  
+    echo "<br/>----------Unit Test----------";
+	  echo "<br/>"."What To Do: ".$_WHAT_TO_DO;
+	  echo "<br/>".$itemO->getItemID();
+  
+  }
+
+
+  /* Sold out item */
+  else if($_WHAT_TO_DO == 'SOLDOUT_ITEM'){
+  
+    /* create new object */
+    $itemO = new Item();
+    
+    /* set object member variable */
+    $itemO->setItemID($_POST["_itemID"]);
+  
+  
+    echo "<br/>----------Unit Test----------";
+	  echo "<br/>"."What To Do: ".$_WHAT_TO_DO;
+	  echo "<br/>".$itemO->getItemID();
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /* ============================================ */
+  if($_WHAT_TO_DO == 'test'){
+  
+	  echo "WHAT_TO_DO: ".$_WHAT_TO_DO;
+
+    /* 잘 작성했는지 테스트 해보기 */
+
+	  /* 생성 */
+	  $abc = new User();
+	  $def = new Admin();
+	  $ghi = new Item();
+	  $jkl = new Cart();
+	  $mno = new Order();
+
+
+    /* test user */
+
+    $abc->setID("kim");
+    $abc->setPswd("1234");
+    $abc->setDate(2018-4-8);
+    $abc->setName("김철수");
+    $abc->setZipCode("ㅁㄴㅇㄹ");
+    $abc->setAddress("cau");
+    $abc->setEmail("kim@naver.com");
+    $abc->setTelNum("010-0000-0000");
+    $abc->setPoint(10000);
+  
+    echo "<br/>----------User Test----------";
+	  echo "<br/>id: ".$abc->getID();
+	  echo "<br/>pswd: ".$abc->getPswd();
+	  echo "<br/>date: ".$abc->getDate();
+	  echo "<br/>name: ".$abc->getName();
+	  echo "<br/>zipcode: ".$abc->getZipCode();
+	  echo "<br/>addr: ".$abc->getAddress();
+	  echo "<br/>email: ".$abc->getEmail();
+	  echo "<br/>tel: ".$abc->getTelNum();
+	  echo "<br/>point: ".$abc->getPoint();
+
+
+  
+    /* test Admin */
+
+    $def->setID("admin01");
+    $def->setPswd("password");
+    $def->setAuthority(3);
+  
+    echo "<br/>----------Admin Test----------";
+	  echo "<br/>id: ".$def->getID();
+	  echo "<br/>pswd: ".$def->getPswd();
+	  echo "<br/>autho: ".$def->getAuthority();
+  
+  }
+  
+
+
+
+
+
+?>
