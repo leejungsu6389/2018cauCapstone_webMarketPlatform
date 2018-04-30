06135faee42d9102a040ce22c38e3858ecdbaef6
leejungsu <33692466+leejungsu6389@users.noreply.github.com>
2018-05-01 02:35:41 +0900

    사용자 계정 모듈 연동 완료

--- user_account.php
+++ user_account.php
@@ -65,8 +65,8 @@
       
       /* using prepared statement */
       /* block the SQL injection */
-      $sql = "INSERT INTO user(userID, pswd, name, joined_date, zipCode,
-              address, email, telNum, mileage) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+      $sql = "INSERT INTO user(userID, pswd, name, joined_date, zipCode, address, email, telNum, mileage) 
+              VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
   
       /* prepare */
       $stmt = $s->prepare($sql);
@@ -78,8 +78,8 @@
       /* set parameters */
       $b_id = $userO->getID();  
       $b_pswd = $userO->getPswd();  
-      $b_name = $userO->getDate();  
-      $b_joined_date = $userO->getName();  
+      $b_name = $userO->getName();  
+      $b_joined_date = $userO->getDate();  
       $b_zipCode = $userO->getZipCode();  
       
       $b_address = $userO->getAddress();  
@@ -166,30 +166,74 @@
   
       /* create new object */
       $userO = new User();
+      
     
       /* set object member variable */
       $userO->setID($_POST["_userID"]);
       $userO->setPswd($_POST["_pswd"]);
-      $userO->setName($_POST["_name"]);
-      $userO->setDate($_POST["_joined_date"]);
+      
+      $new_pswd = $_POST["_pswd2"];
+       
       $userO->setZipCode($_POST["_zipCode"]);
       $userO->setAddress($_POST["_address"]);
       $userO->setEmail($_POST["_email"]);
       $userO->setTelNum($_POST["_telNum"]);
-      $userO->setPoint($_POST["_mileage"]);
+      
+
   
   
+      /*
       echo "<br/>----------Unit Test----------";
 	    echo "<br/>"."What To Do: ".$_WHAT_TO_DO;
 	    echo "<br/>".$userO->getID();
 	    echo "<br/>".$userO->getPswd();
-	    echo "<br/>".$userO->getDate();
-	    echo "<br/>".$userO->getName();
+      
+	    echo "<br/>".$new_pswd;
+      
 	    echo "<br/>".$userO->getZipCode();
 	    echo "<br/>".$userO->getAddress();
 	    echo "<br/>".$userO->getEmail();
 	    echo "<br/>".$userO->getTelNum();
-	    echo "<br/>".$userO->getPoint();
+      */
+      
+      
+      /* import db connection header */
+      require_once("./db_connect.php");
+  
+      
+      /* using prepared statement */
+      /* block the SQL injection */
+      $sql = "UPDATE user
+              SET pswd = ?, zipCode = ?, address = ?, email = ?, telNum = ?
+              WHERE userID = ? AND pswd = ?";
+  
+      /* prepare */
+      $stmt = $s->prepare($sql);
+      
+      
+      /* bind */
+      $stmt->bind_param("sssssss", 
+                        $b_pswd2, $b_zipCode, $b_address, $b_email, $b_telNum,
+                        $b_id, $b_pswd);
+                        
+      /* set parameters */
+      $b_id = $userO->getID();  
+      $b_pswd = $userO->getPswd();  
+      
+      $b_pswd2 = $new_pswd; 
+      
+      $b_zipCode = $userO->getZipCode();      
+      $b_address = $userO->getAddress();  
+      $b_email = $userO->getEmail();  
+      $b_telNum = $userO->getTelNum();  
+      
+      /* execute */
+      $stmt->execute();
+      
+      $stmt->close();
+      
+      /* disconnect db */ 
+      mysqli_close($s);
   
   
     }
@@ -200,17 +244,66 @@
   
       /* create new object */
       $userO = new User();
+ 
     
       /* set object member variable */
       $userO->setID($_POST["_userID"]);
       $userO->setPswd($_POST["_pswd"]);
+      
+      $sig = $_POST["_signature"];
+      
   
-  
+      /*
       echo "<br/>----------Unit Test----------";
 	    echo "<br/>"."What To Do: ".$_WHAT_TO_DO;
 	    echo "<br/>".$userO->getID();
 	    echo "<br/>".$userO->getPswd();
+      
+	    echo "<br/>".$new_pswd;
+      
+	    echo "<br/>".$userO->getZipCode();
+	    echo "<br/>".$userO->getAddress();
+	    echo "<br/>".$userO->getEmail();
+	    echo "<br/>".$userO->getTelNum();
+      */
+      
+      
+      if($sig == '동의'){
+      
+        /* import db connection header */
+        require_once("./db_connect.php");
   
+      
+        /* using prepared statement */
+        /* block the SQL injection */
+        $sql = "DELETE FROM user
+                WHERE userID = ? AND pswd = ?";
+  
+        /* prepare */
+        $stmt = $s->prepare($sql);
+      
+      
+        /* bind */
+        $stmt->bind_param("ss", 
+                          $b_id, $b_pswd);
+                        
+        /* set parameters */
+        $b_id = $userO->getID();  
+        $b_pswd = $userO->getPswd();  
+      
+      
+        /* execute */
+        $stmt->execute();
+      
+        $stmt->close();
+      
+        /* disconnect db */ 
+        mysqli_close($s);
+  
+      }
+      else{
+        print "do nothing";
+      }
   
     }
   
